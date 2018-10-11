3f57390256ff94d4921cd0688816971605cbbf1c
Jcewazhere <jcewazhere@yahoo.com>
2018-10-11 12:38:50 -0500

    Start

---
+++ user/src/main/java/com/revature/user/App.java
@@ -0,0 +1,13 @@
+package com.revature.user;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
